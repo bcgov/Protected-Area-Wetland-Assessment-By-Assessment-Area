9b69854a4919b1d81a5308a67fd8c9f9ad52c3b0
Fraser <Jesse.Fraser@gov.bc.ca>
2020-06-12 11:56:29 -0700

    Currently setup to run in the ArcGIS Python window.  Also, get through the iteration process correctly
    Still adding a weird extra set of percent fields to the table.
    
    Currently setup to run out of the ArcGIS Python.

--- ProtectedArea_WetlandAssessment.py
+++ ProtectedArea_WetlandAssessment.py
@@ -29,19 +29,26 @@ except:
 #Set the time stamp
 time = time.strftime("%y%m%d")
 
+
 #Input Wetland Complex dataset
-wetland_complex_input = arcpy.GetParameterAsText(0)
+#wetland_complex_input = arcpy.GetParameterAsText(0)
+wetland_complex_input = r"V:\srm\smt\Workarea\ArcProj\P17_Skeena_ESI\Data\Values\Wetlands\BaseData\WetlandsBase.gdb\Wetlands_Restart_190911\ESI_basic_Wetland_Complexes_190912"
 
 #Protecte Area input
-protectedAreas = arcpy.GetParameterAsText(1)
+#protectedAreas = arcpy.GetParameterAsText(1)
+protectedAreas = r"V:\srm\smt\Workarea\ArcProj\P17_Skeena_ESI\Data\Values\Wetlands\T1.5\DesignatedLands\SSAF_FWAau_NoHarvest_2018_200408.shp"
+
 #Assessment Unit Input
-AU = arcpy.GetParameterAsText(2)
+#AU = arcpy.GetParameterAsText(2
+AU = r"V:\srm\smt\Workarea\ArcProj\P17_Skeena_ESI\Data\ESI_Data.gdb\AU\SSAF_fwaAU_Watersheds"
 
 #Save Location Folder
-output_save = arcpy.GetParameterAsText(3)
+#output_save = arcpy.GetParameterAsText(3)
+output_save = r"V:\srm\smt\Workarea\ArcProj\P17_Skeena_ESI\Data\Values\Wetlands\T1"
 
 #AU Unique ID Field
-au_ID = arcpy.GetParameterAsText(4)
+#au_ID = arcpy.GetParameterAsText(4)
+au_ID = r"WATERSHED_FEATURE_ID"
 
 #create geodatabase to work out of
 save_gdb = "Wet_Protected_" + time
@@ -51,6 +58,14 @@ output_gdb = output_save + r"\Wet_Protected_" + time + r".gdb"
 #copy the wetland complex into the new gdb
 working_au = output_gdb + r"\au_" + time
 arcpy.CopyFeatures_management(AU, working_au)
+'''
+working_wet = output_gdb + r"\wet_" + time
+test1 = arcpy.CopyFeatures_management(wetland_complex_input, working_wet)
+print test1
+working_prot = output_gdb + r"\prot_" + time
+test2 = arcpy.CopyFeatures_management(protectedAreas, working_prot)
+print test2
+'''
 
 #create Assessment Unit layer to query
 arcpy.MakeFeatureLayer_management(working_au,"au_lyr")
@@ -64,57 +79,39 @@ lyr_wet = arcpy.mapping.Layer("wet_lyr")
 arcpy.MakeFeatureLayer_management(protectedAreas,"prot_lyr")
 lyr_prot = arcpy.mapping.Layer("prot_lyr")
 
+#get the  name 
+desc = arcpy.Describe(wetland_complex_input)
+#Get name to add FID query
+#Also protect from shapefile inputs
+if desc.name[-4:] == ".shp":
+	wetland_name = desc.name[:-4]
+else:
+	wetland_name = desc.name
+#get the name 
+desc = arcpy.Describe(protectedAreas)
+#Get name to add FID query
+if desc.name[-4:] == ".shp":
+	protected_name = desc.name[:-4]
+else:
+	protected_name = desc.name
 
-#add fields to working wetland feature
-protected_Area = "AU_protected_area"
-arcpy.AddField_management(lyr_au, protected_Area, "DOUBLE")
-perc_prot = "Perc_AU_Protected"
-arcpy.AddField_management(lyr_au, perc_prot, "DOUBLE")
 wetland_Area = "AU_wetland_area"
-arcpy.AddField_management(lyr_au, wetland_Area, "DOUBLE")
-perc_Wetland = "Perc_AU_Wetlands"
-arcpy.AddField_management(lyr_au, perc_Wetland, "DOUBLE")
-prot_wet_area = "AU_Protected_wetland_area"
-arcpy.AddField_management(lyr_au, prot_wet_area, "DOUBLE")
-perc_AU_Wetland_prot = "Perc_AU_Protected_Wetlands"
-arcpy.AddField_management(lyr_au, perc_AU_Wetland_prot, "DOUBLE")
-perc_Wetland_prot = "Perc_Wetlands_Protected"
-arcpy.AddField_management(lyr_au, perc_Wetland_prot, "DOUBLE")
+
+#Create FID queries
+prot_FID = r"FID_" + protected_name
+wet_FID = r"FID_" + wetland_name
+
+print wet_FID
+print prot_FID
 
 #get the areafield name to avoid geometry vs shape issue (Thanks you Carol Mahood)
 desc = arcpy.Describe(lyr_au)
 geomField = desc.shapeFieldName
 areaFieldName = str(geomField) + "_Area"
 
-'''
-#get the areafield name to avoid geometry vs shape issue (Thanks you Carol Mahood)
-desc = arcpy.Describe(lyr_wet)
-#Get name to add FID query to
-wetland_name = desc.nameString
-
-#get the areafield name to avoid geometry vs shape issue (Thanks you Carol Mahood)
-desc = arcpy.Describe(lyr_prot)
-#Get name to add FID query to
-protected_name = desc.nameString
-
-#Create FID queries
-prot_FID = r"FID_" + protected_name
-wet_FID = r"FID_" + wetland_name
-
-'''
-
-
-
-#Create FID queries
-prot_FID = r"FID_" + str(lyr_prot)
-wet_FID = r"FID_" + str(lyr_wet)
-
-print wet_FID
-print prot_FID
-
 #Make a Union feature to query for values
 working_union = output_gdb + r"\wet_au_Union_" + time
-arcpy.Union_analysis([lyr_au, lyr_prot, lyr_wet], working_union)
+arcpy.Union_analysis([working_au, protectedAreas, wetland_complex_input], working_union)
 
 #create Wetland layer to query
 arcpy.MakeFeatureLayer_management(working_union,"union_lyr")
@@ -125,68 +122,107 @@ desc = arcpy.Describe(lyr_union)
 geomField = desc.shapeFieldName
 union_areaFieldName = str(geomField) + "_Area"
 
-
+#add fields to working wetland feature
+protected_Area = "AU_protected_area"
+arcpy.AddField_management(lyr_au, protected_Area, "DOUBLE")
+perc_prot = "Perc_ofAU_Protected"
+arcpy.AddField_management(lyr_au, perc_prot, "DOUBLE")
+wetland_Area = "AU_wetland_area"
+arcpy.AddField_management(lyr_au, wetland_Area, "DOUBLE")
+perc_Wetland = "Perc_ofAU_Wetlands"
+arcpy.AddField_management(lyr_au, perc_Wetland, "DOUBLE")
+prot_wet_area = "AU_Protected_wetland_area"
+arcpy.AddField_management(lyr_au, prot_wet_area, "DOUBLE")
+perc_AU_Wetland_prot = "Perc_ofAU_Protected_Wetlands"
+arcpy.AddField_management(lyr_au, perc_AU_Wetland_prot, "DOUBLE")
+perc_Wetland_prot = "Perc_ofWetlands_Protected_ByAU"
+arcpy.AddField_management(lyr_au, perc_Wetland_prot, "DOUBLE")
 
 sumwetland_Area = 0
 sumprotected_Area = 0
 sumwetland_protected_Area = 0
 num_done = 0
 #Iterate through AUs to clip protected area and wetlands
-with arcpy.da.UpdateCursor(lyr_au, [au_ID, protected_Area, wetland_Area, prot_wet_area, perc_prot, perc_Wetland, perc_AU_Wetland_prot, areaFieldName, perc_Wetland_prot]) as cursor:
-	for test in cursor:
-		sumwetland_Area = 0
-		sumprotected_Area = 0
-		sumwetland_protected_Area = 0
+rows = arcpy.UpdateCursor(lyr_au)
+for row in rows:
+	
+	sumwetland_Area = 0
+	sumprotected_Area = 0
+	sumwetland_protected_Area = 0
+	wtrAU = row.getValue(au_ID)	
+	str_test = str(wtrAU)[:-2]
 		
-		str_test = str(test[0])[:-2]
-		
-		#Make sure the Assessment unit is the only feature being red 
-		lyr_au.definitionQuery = au_ID + r" = " + str_test
-		
-			
-		cursor2 = arcpy.SearchCursor(working_union)	
-		''' Protected in AU '''
-		# Def Query for given AU Protected Area
-		lyr_union.definitionQuery = au_ID + r" = " + str_test + "AND " + prot_FID + " <> -1"
-		
-			
+	''' Protected in AU '''
+	# Def Query for given AU Protected Area
+	lyr_union.definitionQuery = au_ID + r" = " + str_test + " AND " + prot_FID + " <> -1"
+	
+	with arcpy.da.UpdateCursor(lyr_union, [union_areaFieldName]) as cursor:		
+		#Iterate through each feature to get the total area
+		for test in cursor:
+			sumprotected_Area = test[0] + sumprotected_Area
+	'''  End   '''
+	lyr_union.definitionQuery = ""
+	''' Wetlands in  AU '''	
+	
+	# Def Query for given AU Wetland Area
+	lyr_union.definitionQuery = au_ID + r" = " + str_test + " AND " + wet_FID + " <> -1"
+	
+	#Iterate through each feature to get the total area 
+	with arcpy.da.UpdateCursor(lyr_union, [union_areaFieldName]) as cursor2:		
 		#Iterate through each feature to get the total area
 		for test2 in cursor2:
-			sumprotected_Area = test2.getValue(union_areaFieldName) + sumprotected_Area
-		'''  End   '''
-		
-		''' Wetlands in  AU '''	
-		# Def Query for given AU Wetland Area
-		lyr_union.definitionQuery = au_ID + r" = " + str_test + "AND " + wet_FID + " <> -1"
-		
-		#Iterate through each feature to get the total area 
-		for test2 in cursor2:
-			sumwetland_Area = test2.getValue(union_areaFieldName) + sumwetland_Area
-		''' End '''
-		
-		''' Wetlands Protected '''
-		# Def Query for given AU Protected Wetland Area
-		lyr_union.definitionQuery = au_ID + r" = " + str_test + "AND " + wet_FID + " <> -1" + "AND " + prot_FID + " <> -1"
-		
-		#Iterate through each feature to get the total area 
-		for test2 in cursor2:
-			sumwetland_protected_Area = test2.getValue(union_areaFieldName) + sumwetland_protected_Area
-		''' End '''
+			sumwetland_Area = test2[0] + sumwetland_Area
 
-		test[1] = sumprotected_Area
-		test[2] = sumwetland_Area
-		test[3] = sumwetland_protected_Area
-		cursor.updateRow(test)
+	''' End '''
+	lyr_union.definitionQuery = ""
 		
-		test[4] = (test[1]/test[7])*100
-		test[5] = (test[2]/test[7])*100
-		test[6] = (test[3]/test[7])*100
-		if test[2] > 0:
-			test[8] = (test[3]/test[2])*100
-		else:
-			test[8] = 0
-		cursor.updateRow(test)
+	''' Wetlands Protected '''
+	# Def Query for given AU Protected Wetland Area
+	lyr_union.definitionQuery = au_ID + r" = " + str_test + " AND " + wet_FID + " <> -1" + " AND " + prot_FID + " <> -1"
+	#Iterate through each feature to get the total area 
+	with arcpy.da.UpdateCursor(lyr_union, [union_areaFieldName]) as cursor3:		
+		#Iterate through each feature to get the total area
+		for test3 in cursor3:
+			sumwetland_protected_Area = test3[0] + sumwetland_protected_Area			
+	''' End '''
+
+	lyr_union.definitionQuery = ""
 		
-		lyr_au.definitionQuery = ""
-		num_done = num_done + 1
-		print "Done Section: " + str(num_done)
+		#Populate the fields
+	areaBig = row.getValue(areaFieldName)
+	row.setValue(wetland_Area, sumwetland_Area)
+	row.setValue(protected_Area, sumprotected_Area)
+	row.setValue(prot_wet_area, sumwetland_protected_Area)
+	
+	
+	test4 = (sumprotected_Area/areaBig)*100
+	row.setValue(perc_prot, test4)
+	
+	test5 = (sumwetland_Area / areaBig)*100
+	row.setValue(perc_Wetland, test5)
+	
+	test6 = (sumwetland_protected_Area / areaBig)*100
+	row.setValue(perc_AU_Wetland_prot, test6)
+	
+	if sumwetland_Area > 0:
+		test8 = (sumwetland_protected_Area/sumwetland_Area)*100
+		row.setValue(perc_Wetland_prot, test8)
+		
+	else:
+		row.setValue(perc_Wetland_prot, 0)
+	rows.updateRow(row)
+		
+	lyr_au.definitionQuery = ""
+	num_done = num_done + 1
+	
+	print "Done Section: " + str(num_done)
+	print "Watershed ID: " + str_test
+	
+	sumwetland_Area = 0
+	sumprotected_Area = 0
+	sumwetland_protected_Area = 0
+		
+	
+
+print "ALL DONE"
+		
